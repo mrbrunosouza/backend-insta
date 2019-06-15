5780bda54b6b66274e377850cd074996c51bbdc1
Bruno Timóteo de Souza <bruno.souza@insidesistemas.com.br>
2019-06-15 20:02:46 -0300

    Primeiro Commit

---
+++ src/controllers/LikeController.js
@@ -0,0 +1,17 @@
+const Post = require('../models/Posts');
+
+module.exports ={
+  
+    async store(req, res){
+        const post = await Post.findById(req.params.id);
+
+        post.likes ++;
+
+        await post.save();
+
+        req.io.emit('like', post);
+
+        return res.json(post);
+
+    }
+};
